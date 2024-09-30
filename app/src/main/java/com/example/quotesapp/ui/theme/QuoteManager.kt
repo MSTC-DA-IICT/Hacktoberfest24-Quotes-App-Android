7cf432bf7c83a0941f98c3d17d6df0666ca55ff7
Parjanya Rajput <pajuraj05@gmail.com>
2024-10-01 03:01:27 +0530

    Update in QuoteManager

--- app/src/main/java/com/example/quotesapp/ui/theme/QuoteManager.kt
+++ app/src/main/java/com/example/quotesapp/ui/theme/QuoteManager.kt
@@ -1,4 +1,11 @@
 package com.example.quotesapp.ui.theme
 
 object QuoteManager {
+
+    // Create a boolean variable named isQuotesLoaded which will be used later
+
+    //Implement the logic for reading and parsing the json file and then convert it to Gson
+
+    //Return the array<Quote>
+
 }
